0e17f924cf34c761813e9c1e164490a57150cb9d
José Luis Medina Sanz <joseluismedina2019@gmail.com>
2026-03-09 13:38:08 +0100

    Completado el punto con la clase mal subida

--- code/src/main/java/com/endes/figuras/punto.java
+++ code/src/main/java/com/endes/figuras/punto.java
@@ -1,4 +1,33 @@
-
 public class punto {
 
+	    private double x;
+	    private double y;
+
+	    public punto(double x, double y) {
+	        this.x = x;
+	        this.y = y;
+	    }
+
+	    public double getX() {
+	        return x;
+	    }
+
+	    public double getY() {
+	        return y;
+	    }
+
+	    public void setX(double x) {
+	        this.x = x;
+	    }
+
+	    public void setY(double y) {
+	        this.y = y;
+	    }
+
+	    public double distancia(punto otro) {
+	        double dx = this.x - otro.getX();
+	        double dy = this.y - otro.getY();
+	        return Math.sqrt(dx * dx + dy * dy);
+	    }
+	}
 }
